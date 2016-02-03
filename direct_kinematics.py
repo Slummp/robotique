63194740163e35f67980e14b80070624e6c451ad
Slummp <alexandrebrouste@gmail.com>
2016-02-03 15:39:55 +0100

    modifs python

--- direct_kinematics.py
+++ direct_kinematics.py
@@ -37,27 +37,3 @@ def leg_dk(theta1, theta2, theta3, l1 = 51, l2 = 63.7, l3 = 93):
     print("P1 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P1))
     print("P2 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P2))
     print("P3 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P3))
-
-def fab(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
-
-    correction_theta2 = -20.69
-    correction_theta3 = 90 + correction_theta2 - 5.06
-
-    theta1 = radians(theta1)
-    theta2 = radians(theta2-correction_theta2)
-    theta3 = radians(-(theta3-correction_theta3))
-
-
-    d12 = l2 * cos(theta2)
-    d23 = l3 * cos(theta2 + theta3)
-
-    plan_contribution = l1 + d12 + d23
-
-
-    x = cos(theta1) * plan_contribution
-    y = sin(theta1) * plan_contribution
-    z = - (l2 * sin(theta2) +l3 * sin(theta2 + theta3))
-
-    print(x)
-    print(y)
-    print(z)
