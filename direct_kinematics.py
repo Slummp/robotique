d8f826fd7bb1fc7fba58caeec265dd784330d7b6
Slummp <alexandrebrouste@gmail.com>
2016-02-06 08:48:23 +0100

    inverse kinematics

--- direct_kinematics.py
+++ direct_kinematics.py
@@ -2,38 +2,36 @@
 
 from math import *
 
-def leg_dk(theta1, theta2, theta3, l1 = 51, l2 = 63.7, l3 = 93):
+
+def leg_dk(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
     theta1 = radians(theta1)
-    theta2 = radians(theta2)
+    theta2 = -radians(theta2)
     theta3 = radians(theta3)
 
     alpha = radians(20.69)
     beta = radians(5.06)
 
-    # newTheta2 = theta2 - alpha
-    # newTheta3 = theta3 + beta
+    new_theta2 = theta2 - alpha
+    new_theta3 = theta3 + beta + alpha - (pi / 2)
 
-    newTheta2 = theta2 + alpha
-    newTheta3 = alpha + (pi / 2) + theta3 + beta
-
-    P1 = {
+    p1 = {
         "x": cos(theta1) * l1,
         "y": sin(theta1) * l1,
         "z": 0
     }
 
-    P2 = {
-        "x": P1["x"] + cos(theta1) * cos(newTheta2) * l2,
-        "y": P1["y"] + sin(theta1) * cos(newTheta2) * l2,
-        "z": P1["z"] + sin(newTheta2) * l2
+    p2 = {
+        "x": p1["x"] + cos(theta1) * cos(new_theta2) * l2,
+        "y": p1["y"] + sin(theta1) * cos(new_theta2) * l2,
+        "z": p1["z"] + sin(new_theta2) * l2
     }
 
-    P3 = {
-        "x": P2["x"] + cos(theta1) * cos(newTheta2 + newTheta3) * l3,
-        "y": P2["y"] + sin(theta1) * cos(newTheta2 + newTheta3) * l3,
-        "z": P2["z"] + sin(newTheta2 + newTheta3) * l3
+    p3 = {
+        "x": p2["x"] + cos(theta1) * cos(new_theta2 + new_theta3) * l3,
+        "y": p2["y"] + sin(theta1) * cos(new_theta2 + new_theta3) * l3,
+        "z": p2["z"] + sin(new_theta2 + new_theta3) * l3
     }
 
-    print("P1 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P1))
-    print("P2 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P2))
-    print("P3 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**P3))
+    print("P1 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**p1))
+    print("P2 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**p2))
+    print("P3 : x:{x:.2f} y:{y:.2f} z:{z:.2f}".format(**p3))
---
+++ functions.py
@@ -0,0 +1,43 @@
+# Alexandre Brouste et Benjamin Bordel
+
+from math import *
+
+
+def pythagore(side1, side2):
+    return sqrt(side1**2 + side2**2)
+
+
+def inverse_pythagore(hypotenuse, side):
+    return sqrt(hypotenuse**2 - side**2)
+
+
+def opposite_from_sine(angle, hypotenuse):
+    return sin(angle) * hypotenuse
+
+
+def hypotenuse_from_sine(angle, opposite):
+    return opposite / sin(angle)
+
+
+def adjacent_from_cosine(angle, hypotenuse):
+    return cos(angle) * hypotenuse
+
+
+def hypotenuse_from_cosine(angle, adjacent):
+    return adjacent / cos(angle)
+
+
+def opposite_from_tangent(angle, adjacent):
+    return tan(angle) * adjacent
+
+
+def adjacent_from_tangent(angle, opposite):
+    return opposite / tan(angle)
+
+
+def al_kashi(angle, side1, side2):
+    return sqrt(side1**2 + side2**2 - 2 * side1 * side2 * cos(angle))
+
+
+def inverse_al_kashi(opposite, side1, side2):
+    return acos((side1**2 + side2**2 - opposite**2) / (2 * side1 * side2))
